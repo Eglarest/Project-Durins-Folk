4177b69af74ec9214fded188b8b8ec578eb2292d
Nicholas Shindler <shindler25@gmail.com>
2018-05-11 17:22:52 -0700

    quick fixes for sh files

--- Dwimorberg/Dunharrow/scripts/stop-database.sh
+++ Dwimorberg/Dunharrow/scripts/stop-database.sh
@@ -6,21 +6,21 @@ if [[ "$os" == "darwin" ]]; then
   # Script to stop an OSX postgreSQL database.
   dbPath=$(PWD)/Dwimorberg/Dimholt;
 
-  echo "\n--------------------  //  -------------------- \nStopping database at " $dbPath " for OSX\n--------------------\n";
+  printf "\n--------------------  //  -------------------- \nStopping database at " $dbPath " for OSX\n--------------------\n";
 
   pg_ctl -D $dbPath stop;
 
-  echo "\n--------------------\nDatabase Stopped\n--------------------  //  -------------------- \n";
+  printf "\n--------------------\nDatabase Stopped\n--------------------  //  -------------------- \n";
 
 elif [[ "$os" == "msys" ]]; then
   # Script to stop an Windows postgreSQL database.
-  echo 'This is windows';
+  printf 'This is windows';
   # add the windows setup script here.
 elif [[ "$os" == "linux" ]]; then
   # Script to stop an Linux postgreSQL database.
-  echo "This is linux";
+  printf "This is linux";
   # add the linux setup script here.
 else
   # Script to let you know you need to add a script for your system.
-  echo 'This is not a supported system';
+  printf 'This is not a supported system';
 fi
--- start-dev-services.sh
+++ start-dev-services.sh
@@ -3,6 +3,12 @@
 ### Run this script to start up dev services
 
 # Startup Dwimorberg Database
-echo "\nStarting database setup.\n";
-sh Argonath/scripts/init-setup.sh;
-echo "\nDatabase setup complete!\n";
+printf "\nStarting database setup.\n";
+
+sh Dwimorberg/Dunharrow/scripts/init-setup.sh;
+
+cd Dwimorberg/Dunharrow/scripts/;
+sh drop-sync-seed.sh;
+cd ../../../;
+
+printf "\nDatabase setup complete!\n";
