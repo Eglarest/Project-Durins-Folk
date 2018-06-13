1c5e73d379f8c9f5c774f338657bd33a5c1007e7
Nicholas Shindler <shindler25@gmail.com>
2018-06-12 17:00:37 -0700

    Getting started

--- Mirrormere/src/App.vue
+++ Mirrormere/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.svg">
     <router-view/>
   </div>
 </template>
@@ -19,5 +18,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
